9102d927da55a9bf6dd8ec2ae13a968f20349a42
vampire-ab <brijeshagal@gmail.com>
2023-12-09 20:38:06 +0530

    new compute

--- bungee_bridge.py
+++ bungee_bridge.py
@@ -5,8 +5,8 @@ from typing import Union
 
 from redis import asyncio as aioredis
 
-from .utils.event_log_decoder import EventLogDecoder
-from .utils.models.message_models import BungeeBridgeSnapshot
+from ....computes.utils.event_log_decoder import EventLogDecoder
+from ....computes.utils.models.message_models import UserDetailsSnapshot
 from snapshotter.utils.callback_helpers import GenericProcessorSnapshot
 from snapshotter.utils.default_logger import logger
 from snapshotter.utils.models.message_models import EthTransactionReceipt
@@ -15,12 +15,12 @@ from snapshotter.utils.redis.redis_keys import epoch_txs_htable
 from snapshotter.utils.rpc import RpcHelper
 
 
-class BungeeBridgeProcessor(GenericProcessorSnapshot):
+class TrackingContractInteraction(GenericProcessorSnapshot):
     transformation_lambdas = None
 
     def __init__(self) -> None:
         self.transformation_lambdas = []
-        self._logger = logger.bind(module='BungeeBridgeProcessor')
+        self._logger = logger.bind(module='TrackingContractInteractionProcessor')
 
     async def compute(
         self,
@@ -28,7 +28,7 @@ class BungeeBridgeProcessor(GenericProcessorSnapshot):
         redis_conn: aioredis.Redis,
         rpc_helper: RpcHelper,
 
-    ) -> Union[None, List[Tuple[str, BungeeBridgeSnapshot]]]:
+    ) -> Union[None, List[Tuple[str, UserDetailsSnapshot]]]:
         min_chain_height = epoch.begin
         max_chain_height = epoch.end
 
@@ -40,7 +40,7 @@ class BungeeBridgeProcessor(GenericProcessorSnapshot):
         txs_hset = await redis_conn.hgetall(epoch_txs_htable(epoch.epochId))
         all_txs = {k.decode(): EthTransactionReceipt.parse_raw(v) for k, v in txs_hset.items()}
 
-        contract_address = '0x555A64968E4803e27669D64e349Ef3d18FCa0895'
+        contract_address = '0xacBF7eAA5bF7c3B52401327edeAc5D8936e45606'
         contract_txs = list(
             map(
                 lambda x: x.dict(), filter(
@@ -50,7 +50,7 @@ class BungeeBridgeProcessor(GenericProcessorSnapshot):
             ),
         )
 
-        with open('snapshotter/modules/computes/static/abis/bungee_bridge_refuel.json') as f:
+        with open('snapshotter/modules/Abi.json') as f:
             abi = json.load(f)
 
         node = rpc_helper.get_current_node()
@@ -68,25 +68,22 @@ class BungeeBridgeProcessor(GenericProcessorSnapshot):
         for tx_receipt in contract_txs:
             for log in tx_receipt['logs']:
                 # Send (address receiver, uint256 amount, bytes32 srcChainTxHash) event log topic
-                if log['topics'][0] == '0x2f824f69f211e444df15d741157e83cdf23c50f39399b9523853a84b91379ca6':
+                if log['topics'][0] == '0x9e2e662eaa46b0fc0ad0689f581ea50987c28baf7c78cda16b01e8979af5423c':
                     try:
                         processed_logs.append(eld.decode_log(log))
                     except:
                         pass
         if processed_logs:
             for log in processed_logs:
-                if log['amount'] > min_amount:
                     snapshots.append(
                         (
                             log['receiver'].lower(),
-                            BungeeBridgeSnapshot(
-                                receiver=log['receiver'],
-                                amount=log['amount'],
-                                srcChainTxHash=log['srcChainTxHash'],
+                            UserDetailsSnapshot(
+                                userId=log['userId'],
+                                heartRate=log['heartRate'],
+                                steps=log['steps'],
+                                sleepDuration=log['sleepDuration']
                             ),
-                        ),
-                    )
-                else:
-                    self._logger.info('Found tx with amount less than min amount, ignoring!')
+                        ),)
 
         return snapshots
--- utils/models/message_models.py
+++ utils/models/message_models.py
@@ -1,90 +1,101 @@
+import uuid
 from typing import Any
 from typing import Dict
 from typing import List
-from typing import Union
+from typing import Optional
 
 from pydantic import BaseModel
+from pydantic import Field
 
 
-class BalanceSnapshot(BaseModel):
-    result: Union[str, Dict[int, str]]
+class TxLogsModel(BaseModel):
+    logIndex: str
+    blockNumber: str
+    blockHash: str
+    transactionHash: str
+    transactionIndex: str
     address: str
-    txs: List[Dict[Any, Any]]
+    data: str
+    topics: List[str]
 
 
-class BungeeBridgeSnapshot(BaseModel):
-    receiver: str
-    amount: int
-    srcChainTxHash: str
 
 
-class QuickswapSwapSnapshot(BaseModel):
-    to: str
-    value: float
+class PowerloomSnapshotProcessMessage(EpochBase):
+    data_source: Optional[str] = None
+    primary_data_source: Optional[str] = None
+    genesis: Optional[bool] = False
+    bulk_mode: Optional[bool] = False
 
 
-class QuickswapLPDepositSnapshot(BaseModel):
-    address: str
-    sender: str
-    shares: int
-    amount0: int
-    amount1: int
 
-
-class QuickswapLPIncreaseLiquiditySnapshot(BaseModel):
-    tokenId: int
-    liquidity: int
-    actualLiquidity: int
-    amount0: int
-    amount1: int
-    pool: str
-
-
-class SafeCreateSnapshot(BaseModel):
-    proxy: str
-    singleton: str
-    initiator: str
-    owners: List[str]
-    threshold: int
-    initializer: str
-
-
-class SafeAddOwnerSnapshot(BaseModel):
-    safe_address: str
-    owner: str
-
-
-class OwltoBridgeSnapshot(BaseModel):
-    receiver: str
-    amount: int
-
-
-class ERC20BalanceSnapshot(BalanceSnapshot):
-    token: str
-
-
-class BlockDetails(BaseModel):
-    difficulty: str
-    extraData: str
-    gasLimit: str
+class EthTransactionReceipt(BaseModel):
+    transactionHash: str
+    transactionIndex: str
+    blockHash: str
+    blockNumber: str
+    from_field: str = Field(..., alias='from')
+    to: Optional[str]
+    cumulativeGasUsed: str
     gasUsed: str
-    hash: str
+    effectiveGasPrice: str
+    logs: List[TxLogsModel]
+    contractAddress: Optional[str] = None
     logsBloom: str
-    miner: str
-    mixHash: str
-    nonce: str
-    number: str
-    parentHash: str
-    receiptsRoot: str
-    sha3Uncles: str
-    size: str
-    stateRoot: str
-    timestamp: str
-    totalDifficulty: str
-    transactions: List[str]
-    transactionsRoot: str
-    uncles: List[str]
+    status: str
+    type: Optional[str]
+    root: Optional[str]
+class DailyReading(BaseModel):
+    heartRate: int
+    steps: int
+    sleepDuration: int
+
+class Stats(BaseModel):
+    height: int
+    weight: int
+
+class UserData(BaseModel):
+    details: List[DailyReading]
+    moreDetails: List[Stats]
+
+class MoreUserDetailsEvent(BaseModel):
+    userId: str
+    height: int
+    weight: int
+
+class UserDetailsEvent(BaseModel):
+    userId: str
+    heartRate: int
+    steps: int
+    sleepDuration: int
+
+class UpdateStatePayload(BaseModel):
+    height: int
+    weight: int
+
+class UpdateDailyReadingPayload(BaseModel):
+    details: DailyReading
+
+class LoomGroomContract(BaseModel):
+    userId: str
+    moreDetails: UpdateStatePayload
+    details: UpdateDailyReadingPayload
+
+class SnapshotBase(BaseModel):
+    contract: str
+    chainHeightRange: Dict[str, int]
+    timestamp: int
+
+class MoreUserDetailsSnapshot(SnapshotBase):
+    userId: str
+    height: int
+    weight: int
 
 
-class BlockDetailsSnapshot(BaseModel):
-    result: BlockDetails
+class UserDetailsSnapshot(SnapshotBase):
+    activities: List[UserDetailsEvent]
+
+class LoomGroomSnapshot(BaseModel):
+    userId: str
+    moreDetails: MoreUserDetailsSnapshot
+    details: UserDetailsSnapshot
